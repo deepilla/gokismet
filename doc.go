193be712275f808b8b754e18b9e6b41a942813ed
deepilla <hello@deepilla.com>
2015-04-24 20:58:52 -0600

    Removed code samples from GoDoc overview.

--- doc.go
+++ doc.go
@@ -1,81 +1,22 @@
 /*
-Package gokismet is a Go implementation of the Akismet anti-spam API.
-See http://akismet.com/development/api/#detailed-docs for the Akismet
-documentation. You will need an Akismet API key to use this library.
+Package gokismet is a Go implementation of the Akismet anti-spam API. It allows
+you to check comments, forum posts, and other user-generated content for spam
+and report missed spam or incorrectly flagged spam to Akismet.
 
-Gokismet provides two main classes:
+Gokismet provides two classes:
 
-1. API is a thin wrapper around Akismet's REST API. For most situations,
-you won't need to use this class directly.
+1. API is a wrapper around Akismet's REST API. Typically you won't use
+this directly.
 
-2. Comment is a higher-level class built on top of API. It includes helper
-functions for the most common Akismet use-cases: blog comments, forum
-posts, contact forms and so on.
+2. Comment is a convenience class built on top of API. It provides helper
+functions that hide the implementation details of the Akismet API.
 
-Example 1. Checking spam with the Comment class:
+Note
 
-    comment, err := gokismet.NewComment("YOUR_API_KEY", "http://www.yourwebsite.com")
-    if err != nil {
-        // Handle the error
-    }
+An Akismet API key is required to use this library.
 
-    comment.SetUserIP("127.0.0.1")
-    comment.SetUserAgent("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6")
-    comment.SetAuthor("A. Commenter")
-    comment.SetEmail("acommenter@aol.com")
-    comment.SetURL("http://www.lovecincodemayo.com")
-    comment.SetPermalink("http://www.yourwebsite.com/2015/05/05/its-cinco-de-mayo/")
-    comment.SetContent("I love Cinco de Mayo!")
-    ...
+Background
 
-    status, err := comment.Check()
-    if err != nil {
-        // Handle the error
-    }
-
-    switch status {
-        case gokismet.StatusNotSpam:
-            fmt.Println("Akismet thinks this is a legit comment")
-        case gokismet.StatusProbableSpam:
-            fmt.Println("Akismet thinks this is spam")
-        case gokismet.StatusDefiniteSpam:
-            fmt.Println("Akismet thinks this is the worst kind of spam")
-    }
-
-Example 2. Checking spam with the API class:
-
-    params := url.Values{
-        "blog",                 {"http://yourwebsite.com"},
-        "user_ip",              {"127.0.0.1"},
-        "user_agent",           {"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"},
-        "comment_author",       {"admin"},
-        "comment_author_email", {"test@test.com"},
-        "comment_author_url",   {"http://www.CheckOutMyCoolSite.com"},
-        "permalink",            {"http://yourwebsite.com/blog/post=1"},
-        "comment_type",         {"comment"},
-        "comment_content",      {"This is the comment text."},
-        ...
-    }
-
-    api := NewAPI()
-
-    err := api.VerifyKey("YOUR_API_KEY", "http://yourwebsite.com")
-    if err != nil {
-        // Handle the error
-    }
-
-    status, err := api.CheckComment(&params)
-    if err != nil {
-        // Handle the error
-    }
-
-    switch status {
-        case gokismet.StatusNotSpam:
-            fmt.Println("Akismet thinks this is a legit comment")
-        case gokismet.StatusProbableSpam:
-            fmt.Println("Akismet thinks this is spam")
-        case gokismet.StatusDefiniteSpam:
-            fmt.Println("Akismet thinks this is the worst kind of spam")
-    }
+See http://akismet.com/development/api/#detailed-docs for the Akismet API docs.
 */
 package gokismet
