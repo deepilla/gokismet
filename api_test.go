0ab99000377bbb9df5223ded4d8219f563b3e73d
deepilla <hello@deepilla.com>
2015-04-30 04:23:58 -0600

    Refactored tests.

---
+++ api_test.go
@@ -0,0 +1,367 @@
+package gokismet
+
+import (
+	"encoding/json"
+	"net/url"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+// Error codes for the initConfig function
+const (
+	// No config file
+	codeMissingConfig = 100 + iota
+	// Couldn't parse the JSON in the config file
+	codeInvalidConfig
+	// Config file does not include the required entries
+	codeIncompleteConfig
+	// Couldn't parse the website in the config file
+	codeInvalidSite
+)
+
+type settings struct {
+	// Akismet API Key
+	APIKey string
+	// Website to test for spam
+	Site string
+	// URL of an article on the provided website
+	// We'll generate one if it isn't supplied
+	Article string
+	// IP Address to send in Akismet requests
+	IP string
+	// User Agent to send in Akismet requests
+	UserAgent string
+	// Whether or not to output Requests/Responses
+	// to stdout
+	Debug bool
+}
+
+var api API         // API object used in tests
+var config settings // Configuration settings, read from a JSON file
+
+func TestMain(m *testing.M) {
+
+	// Read config settings
+	errno := initConfig()
+	if errno != 0 {
+		os.Exit(errno)
+	}
+
+	// Initialise global API object
+	initAPI()
+
+	// Run the jewels...
+	os.Exit(m.Run())
+}
+
+func initAPI() {
+	api.TestMode = true
+	api.UserAgent = "Gokismet.API"
+	if config.Debug {
+		api.Output = os.Stdout
+	}
+}
+
+func initConfig() int {
+	// Open the settings file
+	f, err := os.Open("testconfig.json")
+	if err != nil {
+		return codeMissingConfig
+	}
+	// Use the contents to populate the settings struct
+	err = json.NewDecoder(f).Decode(&config)
+	if err != nil {
+		return codeInvalidConfig
+	}
+	// Check that the settings have been populated correctly
+	if config.APIKey == "" || config.Site == "" || config.IP == "" || config.UserAgent == "" {
+		return codeIncompleteConfig
+	}
+	// If there's no article URL specified in the config, make one up
+	if config.Article == "" {
+		u, err := url.Parse(config.Site)
+		if err != nil {
+			return codeInvalidSite
+		}
+		u.Path = "blog/"
+		u.RawQuery = "p=123"
+
+		config.Article = u.String()
+	}
+
+	return 0
+}
+
+// Return the name of a function in the current stack. Skip is the
+// number of stack frames to ascend, where 0 means the current function
+// (i.e. getFunctionName()), 1 means the calling function, and so on.
+func getFunctionName(skip int) string {
+	pc, _, _, ok := runtime.Caller(skip)
+	if !ok {
+		return "InvalidCaller"
+	}
+
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return "UnknownFunction"
+	}
+
+	s := strings.Split(fn.Name(), ".")
+	return s[len(s)-1]
+}
+
+// Non-spammy comment parameters, used in various tests
+func defaultParams() url.Values {
+	return url.Values{
+		_Site:      {config.Site},
+		_UserIP:    {config.IP},
+		_UserAgent: {config.UserAgent},
+		_Referer:   {"http://www.google.com"},
+		_Page:      {config.Article},
+		_Author:    {"gokismet tester"},
+		_Type:      {"comment"},
+		_Email:     {"hello@example.com"},
+		_URL:       {"http://www.example.com"},
+		_Content:   {"This is an example comment that does not contain anything spammy. In the absence of other settings Akismet should return a negative (non-spam) response for this comment. Cheers..."},
+	}
+}
+
+// Our global API object is in test mode. An is_test flag is added to the
+// query parameters for any method call. Check that the flag is being
+// cleaned up afterwards (i.e. deleted from the query parameters).
+func assert_ParamsDoNotContainTestFlag(t *testing.T, method string, params *url.Values) {
+	if params.Get("is_test") != "" {
+		t.Errorf("%s fail: Test Mode flag was not removed after calling %s", getFunctionName(2), method)
+	}
+}
+
+// The main API methods will not work unless an Akismet API key has been
+// verified. Check that those methods fail before verification. We should
+// get an errKeyNotVerified error.
+func assert_MethodFailsBeforeVerify(t *testing.T, method string, err error) {
+	if err == nil {
+		t.Errorf("%s fail: %s succeeded without a verified key", getFunctionName(2), method)
+		return
+	}
+	if err != errKeyNotVerified {
+		t.Errorf("%s fail: %s returned '%s', expected '%s'", getFunctionName(2), method, err, errKeyNotVerified)
+	}
+}
+
+// If an Akismet API call has required parameters, check that the
+// corresponding gokismet.API method fails without those parameters.
+// We should get a "Missing required field" APIError error.
+func assert_MethodFailsWithoutRequiredField(t *testing.T, method string, err error, fields ...string) {
+	if err == nil {
+		t.Errorf("%s fail: %s succeeded with missing fields %s",
+			getFunctionName(2),
+			method,
+			strings.Join(fields, ", "),
+		)
+		return
+	}
+
+	if e, ok := err.(APIError); ok {
+		for _, field := range fields {
+			if e.Result == "Missing required field: "+field+"." {
+				// Success! We got the error message we expected - return with no error
+				return
+			}
+		}
+	}
+
+	t.Errorf("%s fail: %s returned '%s', expected 'Missing required field: %s.'",
+		getFunctionName(2),
+		method,
+		err,
+		strings.Join(fields, " or "),
+	)
+}
+
+// Check that a spam check returned the spam status we expect.
+func assert_SpamStatusEquals(t *testing.T, method string, expected, status SpamStatus, err error) {
+	if err != nil {
+		t.Errorf("%s fail: %s returned error '%s', expected status '%s'",
+			getFunctionName(2),
+			method,
+			err,
+			expected,
+		)
+		return
+	}
+	if status != expected {
+		t.Errorf("%s fail: %s returned status '%s', expected '%s'",
+			getFunctionName(2),
+			method,
+			status,
+			expected,
+		)
+	}
+}
+
+// Confirm that API methods fail without a verified API key
+func TestKeyNotVerified(t *testing.T) {
+	params := defaultParams()
+
+	// CheckComment should fail if the API key isn't verified
+	_, err := api.CheckComment(&params)
+	assert_MethodFailsBeforeVerify(t, "API.CheckComment", err)
+
+	// SubmitSpam should fail if the API key isn't verified
+	err = api.SubmitSpam(&params)
+	assert_MethodFailsBeforeVerify(t, "API.SubmitSpam", err)
+
+	// SubmitHam should fail if the API key isn't verified
+	err = api.SubmitHam(&params)
+	assert_MethodFailsBeforeVerify(t, "API.SubmitHam", err)
+}
+
+// Confirm key verification/storage
+func TestVerifyKey(t *testing.T) {
+	err := api.VerifyKey(config.APIKey, config.Site)
+	if err != nil {
+		t.Errorf("%s fail: verify '%s' returned '%s'", getFunctionName(1), config.APIKey, err)
+	}
+	if api.key != config.APIKey {
+		t.Errorf("%s fail: api key '%s' has not been stored", getFunctionName(1), config.APIKey)
+	}
+}
+
+// Confirm spam detection
+// From the Akismet docs: To simulate a positive (spam) result, make a
+// comment-check API call with the comment_author set to viagra-test-123
+// and all other required fields populated with typical values.
+func TestDetectSpam(t *testing.T) {
+
+	// Set up params for a positive result
+	params := url.Values{
+		_Site:      {config.Site},
+		_UserIP:    {config.IP},
+		_UserAgent: {config.UserAgent},
+		_Author:    {"viagra-test-123"},
+	}
+
+	// And test them
+	status, err := api.CheckComment(&params)
+	assert_SpamStatusEquals(t, "API.CheckComment", StatusProbableSpam, status, err)
+
+	// Add the discard flag to simulate blatant or "pervasive" spam
+	params.Set("test_discard", "true")
+
+	// And test again
+	status, err = api.CheckComment(&params)
+	assert_SpamStatusEquals(t, "API.CheckComment", StatusDefiniteSpam, status, err)
+
+	// Check that the test mode parameter is being cleaned up
+	assert_ParamsDoNotContainTestFlag(t, "API.CheckComment", &params)
+}
+
+// Confirm ham detection
+// From the Akismet docs: To simulate a negative (not spam) result, make a
+// comment-check API call with the user_role set to administrator and all
+// other required fields populated with typical values.
+func TestDetectHam(t *testing.T) {
+
+	// Set up params for a negative result
+	params := url.Values{
+		_Site:       {config.Site},
+		_UserIP:     {config.IP},
+		_UserAgent:  {config.UserAgent},
+		"user_role": {"administrator"},
+	}
+
+	// And test them
+	status, err := api.CheckComment(&params)
+	assert_SpamStatusEquals(t, "API.CheckComment", StatusNotSpam, status, err)
+
+	// Check that the test mode parameter is being cleaned up
+	assert_ParamsDoNotContainTestFlag(t, "API.CheckComment", &params)
+}
+
+// According to the Akismet docs, blog and user_ip are required parameters
+// for comment-check and user_agent is recommended but not required. Confirm
+// that API.CheckComment reflects this behaviour. Unless the Akismet API changes
+// this test should always pass.
+func TestRequiredFields(t *testing.T) {
+
+	var err error
+	var params url.Values
+
+	// Missing: blog, ip
+	// Should throw an error
+	params = defaultParams()
+	params.Del(_Site)
+	params.Del(_UserIP)
+
+	_, err = api.CheckComment(&params)
+	assert_MethodFailsWithoutRequiredField(t, "API.CheckComment", err, _Site, _UserIP)
+
+	// Missing: blog
+	// Should throw an error
+	params = defaultParams()
+	params.Del(_Site)
+
+	_, err = api.CheckComment(&params)
+	assert_MethodFailsWithoutRequiredField(t, "API.CheckComment", err, _Site)
+
+	// Missing: ip
+	// Should throw an error
+	params = defaultParams()
+	params.Del(_UserIP)
+
+	_, err = api.CheckComment(&params)
+	assert_MethodFailsWithoutRequiredField(t, "API.CheckComment", err, _UserIP)
+
+	// Missing: user agent
+	// Should NOT throw an error
+	params = defaultParams()
+	params.Del(_UserAgent)
+
+	_, err = api.CheckComment(&params)
+	if err != nil {
+		t.Errorf("%s fail: API.CheckComment failed with missing field: %s, returned '%s'",
+			getFunctionName(1),
+			_UserAgent,
+			err,
+		)
+	}
+}
+
+// Confirm spam/ham submission.
+// TODO: We need failing tests for api.SubmitSpam and api.SubmitHam. It's
+// impossible to tell if the calls are working if they always return success.
+func TestSubmit(t *testing.T) {
+	params := defaultParams()
+
+	if err := api.SubmitSpam(&params); err != nil {
+		t.Errorf("%s fail: API.SubmitSpam returned '%s'", getFunctionName(1), err)
+	}
+
+	if err := api.SubmitHam(&params); err != nil {
+		t.Errorf("%s fail: API.SubmitHam returned '%s'", getFunctionName(1), err)
+	}
+
+	// Check that the test mode parameter is being cleaned up
+	assert_ParamsDoNotContainTestFlag(t, "API.SubmitSpam", &params)
+}
+
+// Check spam/ham submission with an empty request body.
+// Akismet's submit-spam and submit-ham methods seemingly never fail, even
+// when passed an empty parameter set. See Test_Submit re failing tests
+// for the submit methods.
+func TestEmptySubmit(t *testing.T) {
+
+	// Exit test mode so we have a completely empty request body.
+	// Not even the is_test parameter is set.
+	api.TestMode = false
+
+	if err := api.SubmitSpam(&url.Values{}); err != nil {
+		t.Errorf("%s fail: API.SubmitSpam (no params) returned '%s'", getFunctionName(1), err)
+	}
+
+	if err := api.SubmitHam(&url.Values{}); err != nil {
+		t.Errorf("%s fail: API.SubmitHam (no params) returned '%s'", getFunctionName(1), err)
+	}
+}
---
+++ comment_test.go
@@ -0,0 +1,211 @@
+package gokismet
+
+import (
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+const commentUserAgent = "Gokismet.Comment"
+
+var comment *Comment
+
+// Check that a Comment has been created successfully and that it has the
+// correct website, comment type, test mode and user agent settings.
+func assert_CommentObjectIsValid(
+	t *testing.T, method string, comment *Comment, err error, testMode bool, userAgent string) {
+
+	if err != nil {
+		t.Errorf("%s fail: %s returned %s", getFunctionName(2), method, err)
+		// We can't continue without a Comment object
+		return
+	}
+	// Check that the site is correctly set
+	if v := comment.params.Get("blog"); v != config.Site {
+		t.Errorf("%s fail: %s returned a Comment with Site '%s', expected '%s'",
+			getFunctionName(2),
+			method,
+			v,
+			config.Site,
+		)
+	}
+	// Check that comment type is correctly set
+	if v, e := comment.params.Get("comment_type"), "comment"; v != e {
+		t.Errorf("%s fail: %s returned a Comment with comment type '%s', expected '%s'",
+			getFunctionName(2),
+			method,
+			v,
+			e,
+		)
+	}
+	// Check that TestMode is correctly set
+	if v := comment.api.TestMode; v != testMode {
+		t.Errorf("%s fail: %s returned a Comment with TestMode '%s', expected '%s'",
+			getFunctionName(2),
+			method,
+			strconv.FormatBool(v),
+			strconv.FormatBool(testMode),
+		)
+	}
+	// Check that UserAgent is correctly set
+	if v := comment.api.UserAgent; v != userAgent {
+		t.Errorf("%s fail: %s returned a Comment with UserAgent '%s', expected '%s'",
+			getFunctionName(2),
+			method,
+			v,
+			userAgent,
+		)
+	}
+}
+
+// Check that a Comment has the string value we expect for the given parameter type.
+func assert_StringParameterEquals(t *testing.T, comment *Comment, key string, expected string) {
+	if v := comment.params.Get(key); v != expected {
+		t.Errorf("%s fail: Parameter '%s' is '%s', expected '%s'",
+			getFunctionName(2),
+			key,
+			v,
+			expected,
+		)
+	}
+}
+
+// Check that a Comment has the date value we expect for the given parameter type.
+func assert_DateParameterEquals(t *testing.T, comment *Comment, key string, expected time.Time) {
+	assert_StringParameterEquals(t, comment, key, formatTime(expected))
+}
+
+// Confirm that the constructors create Comments with the expected settings.
+// Note: The last version will be the Comment that gets used in subsequent tests.
+func TestCommentCreate(t *testing.T) {
+	var err error
+
+	// Test all of the Comment constructors
+	comment, err = NewComment(config.APIKey, config.Site)
+	assert_CommentObjectIsValid(t, "Comment.NewComment", comment, err, false, "")
+
+	comment, err = NewTestComment(config.APIKey, config.Site)
+	assert_CommentObjectIsValid(t, "Comment.NewTestComment", comment, err, true, "")
+
+	comment, err = NewCommentUA(config.APIKey, config.Site, commentUserAgent)
+	assert_CommentObjectIsValid(t, "Comment.NewCommentUA", comment, err, false, commentUserAgent)
+
+	comment, err = NewTestCommentUA(config.APIKey, config.Site, commentUserAgent)
+	assert_CommentObjectIsValid(t, "Comment.NewTestCommentUA", comment, err, true, commentUserAgent)
+
+	// Add debugging
+	if config.Debug {
+		comment.LogTo(os.Stdout)
+	}
+}
+
+// Confirm that the Set... functions work as expected.
+// Note: The values used in this function should be non-spammy - we'll check
+// them for spam in later tests and we expect a negative result.
+func TestCommentParameters(t *testing.T) {
+	var s string
+	var ts time.Time
+
+	if comment == nil {
+		// We can't continue without a Comment object
+		t.Fatalf("%s fail: The global Comment object has not been initialised.", getFunctionName(1))
+	}
+
+	comment.SetUserIP(config.IP)
+	assert_StringParameterEquals(t, comment, "user_ip", config.IP)
+
+	comment.SetUserAgent(config.UserAgent)
+	assert_StringParameterEquals(t, comment, "user_agent", config.UserAgent)
+
+	s = "http://www.google.com"
+	comment.SetReferer(s)
+	assert_StringParameterEquals(t, comment, "referrer", s)
+
+	comment.SetPage(config.Article)
+	assert_StringParameterEquals(t, comment, "permalink", config.Article)
+
+	// Set article timestamp to 1 month ago
+	ts = time.Now().AddDate(0, -1, 0)
+	comment.SetPageTimestamp(ts)
+	assert_DateParameterEquals(t, comment, "comment_post_modified_gmt", ts)
+
+	s = "gokismet test"
+	comment.SetAuthor(s)
+	assert_StringParameterEquals(t, comment, "comment_author", s)
+
+	// Specifiying the email address has side effects. It seems to make
+	// the following submit-ham call work, so when this test is run more
+	// than once, the spammy comment is not detected as spam any more.
+	// We can get round this by using a different email for the submit
+	// tests.
+	s = "check@example.com"
+	comment.SetEmail(s)
+	assert_StringParameterEquals(t, comment, "comment_author_email", s)
+
+	s = "This is an example comment that does not contain anything spammy. In the absence of other dodgy settings Akismet should return a negative (non-spam) response..."
+	comment.SetContent(s)
+	assert_StringParameterEquals(t, comment, "comment_content", s)
+
+	s = "http://www.example.com"
+	comment.SetURL(s)
+	assert_StringParameterEquals(t, comment, "comment_author_url", s)
+
+	// Set comment timestamp to current time
+	ts = time.Now()
+	comment.SetTimestamp(ts)
+	assert_DateParameterEquals(t, comment, "comment_date_gmt", ts)
+
+	s = "en_us"
+	comment.SetSiteLanguage(s)
+	assert_StringParameterEquals(t, comment, "blog_lang", s)
+
+	s = "UTF-8"
+	comment.SetCharset(s)
+	assert_StringParameterEquals(t, comment, "blog_charset", s)
+}
+
+// Confirm that spam checks return the expected results.
+func TestCommentCheck(t *testing.T) {
+	if comment == nil {
+		// We can't continue without a Comment object
+		t.Fatalf("%s fail: The global Comment object has not been initialised.", getFunctionName(1))
+	}
+
+	// Test the non-spammy comment set up in the previous function
+	status, err := comment.Check()
+	assert_SpamStatusEquals(t, "Comment.Check", StatusNotSpam, status, err)
+
+	// Make the comment spammy
+	comment.SetAuthor("viagra-test-123")
+
+	// And test again
+	status, err = comment.Check()
+	assert_SpamStatusEquals(t, "Comment.Check", StatusProbableSpam, status, err)
+
+	// Make the comment non-spammy again
+	comment.SetAuthor("gokismet test")
+
+	// And test a final time
+	status, err = comment.Check()
+	assert_SpamStatusEquals(t, "Comment.Check", StatusNotSpam, status, err)
+}
+
+func TestCommentReport(t *testing.T) {
+	if comment == nil {
+		// We can't continue without a Comment object
+		t.Fatalf("%s fail: The global Comment object has not been initialised.", getFunctionName(1))
+	}
+
+	// Reset the email address, otherwise these calls might
+	// affect subsequent calls to Comment.Check.
+	comment.SetEmail("submit@example.com")
+
+	if err := comment.ReportSpam(); err != nil {
+		t.Errorf("%s fail: Comment.ReportSpam returned '%s'", getFunctionName(1), err)
+	}
+
+	if err := comment.ReportNotSpam(); err != nil {
+		t.Errorf("%s fail: Comment.ReportNotSpam returned '%s'", getFunctionName(1), err)
+	}
+}
